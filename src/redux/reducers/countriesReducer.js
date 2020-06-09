1e656592e06fbebd859578645b2cb4095b7793c1
jukeShy <cocax@rambler.ru>
2020-06-09 22:34:04 +0300

    Add countries

--- src/redux/reducers/countriesReducer.js
+++ src/redux/reducers/countriesReducer.js
@@ -1,32 +1,22 @@
-const initialState = [
-  {
-    name: 'USA',
-    confirmed: {
-      value: 1965685,
-    },
-    recovered: {
-      value: 518522,
-    },
-    deaths: {
-      value: 111194,
-    },
-  },
-  {
-    name: 'Russia',
-    confirmed: {
-      value: 1965685,
-    },
-    recovered: {
-      value: 518522,
-    },
-    deaths: {
-      value: 111194,
-    },
-  },
-];
+import {
+  GET_COUNTRIES,
+  COUNTRIES_LOADING_TRUE,
+  COUNTRIES_LOADING_FALSE,
+} from '~/redux/actions/countriesActions/types';
+
+const initialState = {
+  isLoading: false,
+  items: [],
+};
 
 const countriesReduces = (state = initialState, { type, payload }) => {
   switch (type) {
+    case COUNTRIES_LOADING_TRUE:
+      return { ...state, isLoading: true };
+    case COUNTRIES_LOADING_FALSE:
+      return { ...state, isLoading: false };
+    case GET_COUNTRIES:
+      return { ...state, items: [...state.items, ...payload] };
     default:
       return state;
   }
