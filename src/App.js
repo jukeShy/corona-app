0487e8bc2213160aae13f1e51f42a37a90a110c3
jukeShy <cocax@rambler.ru>
2020-06-09 21:16:48 +0300

    Add initial corona api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Home } from '~/pages';
+import { CoronaApi } from './api/corona.api';
 
 const App = () => {
+  const coronaApi = CoronaApi();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await coronaApi.getCountries();
+
+      console.log(data);
+    };
+
+    fetchData();
+  }, [coronaApi]);
+
   return (
     <>
       <Home />
